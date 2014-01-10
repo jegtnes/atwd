3a0d8456b9470fd413a0ce11ca2b961b4cb8a34c
Alex Jegtnes <saccosekk@gmail.com>
2014-01-10 14:31:57 +0000

    Stuffs. I suck at comitting

--- index.php
+++ index.php
@@ -1,9 +1,38 @@
 <pre>
 <?php
-if (($file = fopen("data/data.csv", "r")) === true) {
-    $data = fgetcsv($file);
-    var_dump($data);
-    fclose($file);
+function twoColCsvToArray($filename) {
+	if (!file_exists($filename) || !is_readable($filename)) {
+		return false;
+	}
+
+	$header = null;
+	$data = array();
+	$handle = fopen($filename, 'r');
+
+	if ($handle) {
+		while ($row = fgetcsv($handle, 0, ',')) {
+
+			//assign header name
+			$name = $row[0];
+
+			//remove header name from data
+			array_shift($row);
+
+			//if first iteration of loop, we're on headers
+			if(!$header) {
+				$header = $row;
+			}
+
+			else {
+				$data[$name] = array_combine($header, $row);
+			}
+		}
+		fclose($handle);
+	}
+
+
+	return $data;
 }
+print_r(twoColCsvToArray('./data/dawg.csv'));
 ?>
 </pre>
