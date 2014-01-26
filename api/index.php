77ce4f43b18c52c36f2a0aa4bfdd349634b654ec
Alex Jegtnes <saccosekk@gmail.com>
2014-01-26 21:00:49 +0000

    Removed superflous parameter

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/xml');
 require ("../library/utilities.php");
 
-function createBaseCrimeXml($sourceData, $year) {
+function createBaseCrimeXml($year) {
 	$xml = new SimpleXMLElement("<response></response>");
 	$xml->addAttribute('timestamp', time());
 	$response = $xml->addChild('crimes');
@@ -16,7 +16,7 @@ if (file_exists(DATA_SOURCE)) {
 
 	$request = parseApiRequest($_SERVER['REQUEST_URI']);
 
-	$crimes = createBaseCrimeXml(DATA_SOURCE, $request['year']);
+	$crimes = createBaseCrimeXml($request['year']);
 
 	foreach ($sourceData->region as $x) {
 		$element = $crimes->addChild('region');
