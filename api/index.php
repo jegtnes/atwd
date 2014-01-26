4e9a9d5bcb522abaedfaef12163d8ae0e2def172
Alex Jegtnes <saccosekk@gmail.com>
2014-01-26 01:08:54 +0000

    Starting to get data from XML file

--- api/index.php
+++ api/index.php
@@ -1,11 +1,23 @@
 <pre>
 <?php
 require ("../library/utilities.php");
-
+function object_merge($o1, $o2)
+{
+return (object) array_merge((array) $o1, (array) $o2);
+}
 if (file_exists(DATA_SOURCE)) {
 	$xml = simplexml_load_file(DATA_SOURCE);
 	$request = parseApiRequest($_SERVER['REQUEST_URI']);
-}
 
+	foreach ($xml->region as $x) {
+		var_dump($x['total']);
+		var_dump($x['id']);
+	}
+
+	foreach ($xml->national as $x) {
+		var_dump($x['total']);
+		var_dump($x['id']);
+	}
+}
 ?>
 </pre>
