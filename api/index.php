ad41588435cdb5ffd9c46a604c5e65526e04e3b8
Alex Jegtnes <saccosekk@gmail.com>
2014-01-26 14:28:34 +0000

    Basic GET request working

--- api/index.php
+++ api/index.php
@@ -1,23 +1,33 @@
-<pre>
 <?php
+header('Content-Type: text/xml');
 require ("../library/utilities.php");
-function object_merge($o1, $o2)
-{
-return (object) array_merge((array) $o1, (array) $o2);
-}
+
 if (file_exists(DATA_SOURCE)) {
-	$xml = simplexml_load_file(DATA_SOURCE);
+	$sourceData = simplexml_load_file(DATA_SOURCE);
+
 	$request = parseApiRequest($_SERVER['REQUEST_URI']);
 
-	foreach ($xml->region as $x) {
-		var_dump($x['total']);
-		var_dump($x['id']);
+	$responseXml = new SimpleXMLElement("<response></response>");
+	$responseXml->addAttribute('timestamp', time());
+	$crimes = $responseXml->addChild('crimes');
+	$crimes->addAttribute('year', $request['year']);
+
+	foreach ($sourceData->region as $x) {
+		$element = $crimes->addChild('region');
+		$element->addAttribute('id', $x['id']);
+		$element->addAttribute('total', $x['total']);
 	}
 
-	foreach ($xml->national as $x) {
-		var_dump($x['total']);
-		var_dump($x['id']);
+	foreach ($sourceData->national as $x) {
+		$element = $crimes->addChild('national');
+		$element->addAttribute('id', $x['id']);
+		$element->addAttribute('total', $x['total']);
 	}
+
+	foreach ($sourceData->country as $x) {
+		$element = $crimes->addChild(lcfirst($x['id']));
+		$element->addAttribute('total', $x['total']);
+	}
+	echo $responseXml->asXML();
 }
 ?>
-</pre>
