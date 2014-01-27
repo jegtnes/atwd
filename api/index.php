f906e04c87dc66ee87aec674cbe210ecbeb12f2b
Alex Jegtnes <saccosekk@gmail.com>
2014-01-27 11:13:08 +0000

    Converted to DOMDocument. JESUS

--- api/index.php
+++ api/index.php
@@ -13,15 +13,45 @@ function createBaseCrimeXml($year) {
 	return $xml;
 }
 
+function returnAllCrime($sourceData, $json = false) {
+	$crimeXml = new DOMDocument;
+	$crimeXml->load($sourceData);
+	$data = $crimeXml->createDocumentFragment();
+	$regions = $crimeXml->getElementsByTagName('region');
+	$nationals = $crimeXml->getElementsByTagName('national');
+	$countries = $crimeXml->getElementsByTagName('country');
+
+	foreach ($regions as $region) {
+		$element = $crimeXml->createElement('region');
+		$element->setAttribute("id", $region->attributes->getNamedItem("id")->nodeValue);
+		$element->setAttribute("total", $region->attributes->getNamedItem("total")->nodeValue);
+		$data->appendChild($element);
+	}
+
+	foreach ($nationals as $national) {
+		$element = $crimeXml->createElement('national');
+		$element->setAttribute("id", $national->attributes->getNamedItem("id")->nodeValue);
+		$element->setAttribute("total", $national->attributes->getNamedItem("total")->nodeValue);
+		$data->appendChild($element);
+	}
+
+	foreach ($countries as $country) {
+		$element = $crimeXml->createElement(lcfirst($country->attributes->getNamedItem("id")->nodeValue));
+		$element->setAttribute("total", $country->attributes->getNamedItem("total")->nodeValue);
+		$data->appendChild($element);
+	}
+	return $data;
+}
+
 if (file_exists(DATA_SOURCE)) {
 
-	$sourceData = simplexml_load_file(DATA_SOURCE);
-
 	$request = parseApiRequest($_SERVER['REQUEST_URI']);
-
 	$crimes = createBaseCrimeXml($request['year']);
-	echo $crimes->saveXML();
 
-	// echo $crimes->saveXML();
+	$crime = returnAllCrime(DATA_SOURCE);
+	$crime = $crimes->importNode($crime, true);
+	$crimes->documentElement->appendChild($crime);
+
+	echo $crimes->saveXML();
 }
 ?>
