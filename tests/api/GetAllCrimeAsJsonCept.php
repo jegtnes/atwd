4cc41e835784dd709ae7517ca6595f66c070a0d5
Alex Jegtnes <saccosekk@gmail.com>
2014-02-23 15:03:55 +0000

    Added test asssertions for various bits of data

--- tests/api/GetAllCrimeAsJsonCept.php
+++ tests/api/GetAllCrimeAsJsonCept.php
@@ -1,6 +1,14 @@
 <?php
+$timestamp = time();
 $I = new ApiGuy($scenario);
 $I->wantTo('View an overview of all crime as JSON');
 $I->sendGET('json');
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
+$I->seeResponseContainsJson(array('timestamp' => "$timestamp"));
+$I->seeResponseContains('"region": [');
+$I->seeResponseContains('"id": "South West",');
+$I->seeResponseContains('"id": "London",');
+$I->seeResponseContains('"national": [');
+$I->seeResponseContains('"england": {');
+$I->seeResponseContains('"wales": {');
