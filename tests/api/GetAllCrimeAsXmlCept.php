554e57d535433bcdb65cd0ef7ded5f9e310113a1
Alex Jegtnes <saccosekk@gmail.com>
2014-02-23 13:29:37 +0000

    Testing XML response, and timestamp

--- tests/api/GetAllCrimeAsXmlCept.php
+++ tests/api/GetAllCrimeAsXmlCept.php
@@ -1,4 +1,8 @@
 <?php
+$timestamp = time();
 $I = new ApiGuy($scenario);
 $I->wantTo('View an overview of all crime as XML');
+$I->sendGET('xml');
 $I->seeResponseCodeIs(200);
+$I->seeResponseIsXml();
+$I->seeResponseContains("<response timestamp=\"$timestamp\">");
