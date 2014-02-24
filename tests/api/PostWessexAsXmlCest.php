1401eea32103c45ea5e66737f93676095243a015
Alex Jegtnes <saccosekk@gmail.com>
2014-02-24 21:29:12 +0000

    Fixed typo in Post tests

--- tests/api/PostWessexAsXmlCest.php
+++ tests/api/PostWessexAsXmlCest.php
@@ -15,7 +15,7 @@ class PostWessexAsXmlCest
     // tests
     public function postWessexAsXml(ApiGuy $I) {
     	$violenceWithoutInjury = rand(1,100);
-    	$violenceWithInjury = rand(1,$violenceWithInjury);
+    	$violenceWithInjury = rand(1,$violenceWithoutInjury);
     	$homicide = rand(1,$violenceWithInjury);
     	$I->wantTo('Create a new area in the South West called Wessex and see a response as XML');
     	$I->sendGET('south_west/wessex/hom:4-vwi:15-vwoi:25/xml');
