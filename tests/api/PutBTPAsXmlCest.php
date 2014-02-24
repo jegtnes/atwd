2369292ecd35e429ef8a42c1d21d117e160febd1
Alex Jegtnes <saccosekk@gmail.com>
2014-02-24 10:51:27 +0000

    Fixed silly issue with codeception not doing stuff in isolation

--- tests/api/PutBTPAsXmlCest.php
+++ tests/api/PutBTPAsXmlCest.php
@@ -17,7 +17,6 @@ class PutBTPAsXmlCest
 		$I->seeResponseContains('<region id="British Transport Police" total="' . $this->randomNumber . '" previous=');
 		$response = (string) $I->grabResponse();
 		preg_match("/total=\"([0-9]*)\" previous=\"([0-9]*)\"/", $response, $amounts);
-		$this->previous = $amounts[2];
-    }
-
+		if ($amounts) $this->previous = $amounts[2];
+	}
 }
