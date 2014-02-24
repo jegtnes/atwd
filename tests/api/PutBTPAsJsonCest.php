8c9c9c94282d335bcb7371efccaaa5285ea62e36
Alex Jegtnes <saccosekk@gmail.com>
2014-02-24 18:01:23 +0000

    Adding test for PUT request with JSON response

---
+++ tests/api/PutBTPAsJsonCest.php
@@ -0,0 +1,50 @@
+<?php
+use \ApiGuy;
+
+class PutBTPAsJsonCest
+{
+
+	public $randomNumber;
+	public $previous;
+	public $timestamp;
+
+	public function _before()
+	{
+		$this->timestamp = time();
+	}
+
+	public function _after()
+	{
+		unset($this->timestamp);
+	}
+
+	public function testOriginal(ApiGuy $I) {
+		$timestamp = time();
+		$this->randomNumber = rand(1,999999);
+		$I->wantTo('PUT update the total amount of BTP and receive a JSON result');
+		$I->sendGET('put/british_transport_police:' . $this->randomNumber . '/json');
+		$I->seeResponseCodeIs(200);
+		$I->seeResponseIsJson();
+		$I->seeResponseContainsJson(array('timestamp' => "$timestamp"));
+		$I->seeResponseContains('"region": {');
+		$I->seeResponseContains('"id": "British Transport Police",');
+		$I->seeResponseContains('"total": "' . $this->randomNumber . '",');
+		$response = (string)($I->grabResponse());
+
+		preg_match("/\"previous\": ?\"([0-9]*)\"/", $response, $amounts);
+		$I->printAVariable($amounts);
+		if ($amounts) $this->previous = $amounts[1];
+	}
+
+	// public function testResetValue(ApiGuy $I) {
+	// 	$timestamp = time();
+	// 	$I->wantTo('PUT update the total amount of BTP back to the original value and receive an XML result');
+	// 	$I->sendGET('put/british_transport_police:' . $this->previous . '/xml');
+	// 	$I->seeResponseCodeIs(200);
+	// 	$I->seeResponseIsXml();
+	// 	$I->seeResponseContains('<region id="British Transport Police" total="' . $this->previous . '" previous="' . $this->randomNumber . '"/>');
+	// 	unset($this->randomNumber);
+	// 	unset($this->previous);
+	// }
+
+}
