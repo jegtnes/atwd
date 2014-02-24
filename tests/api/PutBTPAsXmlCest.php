079372f84092cc6a86631c0d4723ab9376b74766
Alex Jegtnes <saccosekk@gmail.com>
2014-02-24 10:19:36 +0000

    Storing previous variable to use to revert to after

--- tests/api/PutBTPAsXmlCest.php
+++ tests/api/PutBTPAsXmlCest.php
@@ -16,14 +16,16 @@ class PutBTPAsXmlCest
     {
     }
 
-    // tests
-    public function tryToTest(ApiGuy $I) {
+    public function testOriginal(ApiGuy $I) {
     	$timestamp = time();
 		$I->wantTo('PUT update the total amount of BTP and receive an XML result');
 		$I->sendGET('put/british_transport_police:' . $this->randomNumber . '/xml');
 		$I->seeResponseCodeIs(200);
 		$I->seeResponseIsXml();
 		$I->seeResponseContains('<region id="British Transport Police" total="' . $this->randomNumber . '" previous=');
+		$response = (string) $I->grabResponse();
+		preg_match("/total=\"([0-9]*)\" previous=\"([0-9]*)\"/", $response, $amounts);
+		$this->previous = $amounts[2];
     }
 
 }
