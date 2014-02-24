516f0c5465d9f888af68912ec4266994abf5bc34
Alex Jegtnes <saccosekk@gmail.com>
2014-02-24 10:52:00 +0000

    Adding test to put values back to what they were for a PUT request

--- tests/api/PutBTPAsXmlCest.php
+++ tests/api/PutBTPAsXmlCest.php
@@ -19,4 +19,15 @@ class PutBTPAsXmlCest
 		preg_match("/total=\"([0-9]*)\" previous=\"([0-9]*)\"/", $response, $amounts);
 		if ($amounts) $this->previous = $amounts[2];
 	}
+
+	public function testResetValue(ApiGuy $I) {
+		$timestamp = time();
+		$I->wantTo('PUT update the total amount of BTP back to the original value and receive an XML result');
+		$I->sendGET('put/british_transport_police:' . $this->previous . '/xml');
+		$I->seeResponseCodeIs(200);
+		$I->seeResponseIsXml();
+		$I->seeResponseContains('<region id="British Transport Police" total="' . $this->previous . '" previous="' . $this->randomNumber . '"/>');
+		unset($this->randomNumber);
+		unset($this->previous);
+	}
 }
