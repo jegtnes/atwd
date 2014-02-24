bf0b35d77f7c9857f9379b7b351a5b6bf40e0ff4
Alex Jegtnes <saccosekk@gmail.com>
2014-02-24 21:28:26 +0000

    Adding basic test for POST

---
+++ tests/api/PostWessexAsXmlCest.php
@@ -0,0 +1,25 @@
+<?php
+use \ApiGuy;
+
+class PostWessexAsXmlCest
+{
+
+    public function _before()
+    {
+    }
+
+    public function _after()
+    {
+    }
+
+    // tests
+    public function postWessexAsXml(ApiGuy $I) {
+    	$violenceWithoutInjury = rand(1,100);
+    	$violenceWithInjury = rand(1,$violenceWithInjury);
+    	$homicide = rand(1,$violenceWithInjury);
+    	$I->wantTo('Create a new area in the South West called Wessex and see a response as XML');
+    	$I->sendGET('south_west/wessex/hom:4-vwi:15-vwoi:25/xml');
+    	$I->seeResponseCodeIs(200);
+    	$I->seeResponseIsXml();
+    }
+}
