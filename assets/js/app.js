096fe2259720c124cfa60d484a4259a35ac800db
Alex Jegtnes <saccosekk@gmail.com>
2014-02-26 21:13:19 +0000

    Adding select region functionality to datavis

--- assets/js/app.js
+++ assets/js/app.js
@@ -13146,5 +13146,12 @@ return jQuery;
 }(jQuery, this, this.document));
 
 $(document).ready(function() {
-	console.log($('#region'));
+	$.getJSON("http://atwd.dev/crimes/6-2013/json", function(data) {
+		$.each(data.response.crimes.region, function(key, value) {
+			var itemId = value.id;
+			itemId = itemId.replace(/\s/i, "_").toLowerCase();
+			$('#region').append($("<option></option").attr("value", itemId).text(value.id));
+		});
+		// console.log(regions);
+	});
 });
--- datavisualisation.php
+++ datavisualisation.php
@@ -6,7 +6,28 @@
 	<script src="assets/js/app-head.min.js"></script>
 </head>
 <body>
-
-	<script src="assets/js/app.min.js"></script>
+	<div class="contain-to-grid">
+		<nav class="top-bar" data-topbar>
+			<ul class="title-area">
+				<li class="name">
+					<h1><a href="#">Advanced Topics in Web Development</a></h1>
+				</li>
+			</ul>
+			<section class=	"top-bar-section">
+				<ul class="right">
+				</ul>
+			</section>
+		</nav>
+	</div>
+	<div class="row">
+		<div class="columns">
+			<h1>Data Visualisation</h1>
+			<form action="">
+				<label for="region">Select region</label>
+				<select name="region" id="region"></select>
+			</form>
+		</div>
+	</div>
+	<script src="assets/js/app.js"></script>
 </body>
 </html>
