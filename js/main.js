f77be637b85e19598725d00350cee54c4144a99b
Alex Jegtnes <saccosekk@gmail.com>
2014-02-26 22:04:40 +0000

    Relative URL

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	var select = $('#region');
-	$.getJSON("http://atwd.dev/crimes/6-2013/json", function(data) {
+	$.getJSON("crimes/6-2013/json", function(data) {
 		function appendRegionToList(value, elem) {
 			id = value.replace(/\s/i, "_").toLowerCase();
 			elem.append($("<option></option").attr("value", id).text(value));
