ffa13bee87cfc1ffd765a842500f92921e8aee1b
Alex Jegtnes <saccosekk@gmail.com>
2014-02-26 21:41:12 +0000

    DRYup action

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
+	var select = $('#region');
 	$.getJSON("http://atwd.dev/crimes/6-2013/json", function(data) {
+		function appendRegionToList(value, elem) {
+			id = value.replace(/\s/i, "_").toLowerCase();
+			elem.append($("<option></option").attr("value", id).text(value));
+		}
+
 		$.each(data.response.crimes.region, function(key, value) {
-			var itemId = value.id;
-			itemId = itemId.replace(/\s/i, "_").toLowerCase();
-			$('#region').append($("<option></option").attr("value", itemId).text(value.id));
+			appendRegionToList(value.id, select);
 		});
-		// console.log(regions);
+		$.each(data.response.crimes.national, function(key, value) {
+			appendRegionToList(value.id, select);
+		});
 	});
 });
