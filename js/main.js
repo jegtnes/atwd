e06877b3e554b23842589dd8f22ec446c872c4a9
Alex Jegtnes <saccosekk@gmail.com>
2014-02-27 00:03:30 +0000

    Adding pie chart

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
 
 			// on completion, replace the canvases in order to clear the data
 			$('#bar').replaceWith('<canvas id="bar" width="600" height="400"></canvas>');
-			// $('#pie').replaceWith('<canvas id="pie" width="600" height="400"></canvas>');
+			$('#pie').replaceWith('<canvas id="pie" width="600" height="400"></canvas>');
 			var barCanvasContext = $('#bar').get(0).getContext("2d");
 			var barData = {
 				labels : chartLabels,
@@ -45,6 +45,19 @@ $(document).ready(function() {
 				]
 			};
 			new Chart(barCanvasContext).Bar(barData,{});
+
+			var pieCanvasContext = $('#pie').get(0).getContext("2d");
+
+			var pieData = [];
+			$.each(chartData, function(key, value) {
+				// http://www.paulirish.com/2009/random-hex-color-code-snippets/
+				randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+				pieData.push({value: parseInt(value, 10), color: randomColor});
+			});
+
+			console.log(pieData);
+
+			new Chart(pieCanvasContext).Pie(pieData,{});
 		});
 	});
 });
