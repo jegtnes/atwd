1da354aac35a347e884fa6289c081daa09e5c15e
Alex Jegtnes <saccosekk@gmail.com>
2014-02-27 13:57:50 +0000

    stuffs

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	var select = $('#region');
 	$.getJSON("crimes/6-2013/json", function(data) {
 		function appendRegionToList(value, elem) {
-			id = value.replace(/\s/ig	, "_").toLowerCase();
+			id = value.replace(/\s/ig, "_").toLowerCase();
 			elem.append($("<option></option").attr("value", id).text(value));
 		}
 
